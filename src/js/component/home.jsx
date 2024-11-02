21bf551b725fdcac3fc4aeffbcf0607ffbb4abce
María José Fonseca Castro <mfonsecac052@gmail.com>
2024-11-02 02:28:46 +0000

    Todo diseño montado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -18,34 +18,39 @@ const Home = () => {
   }
 
   return (
-    <form onSubmit={sendData}>
-      <h1 className="text-center">To Do List</h1>
-      <div className="Lista">
-        <input type="text" className="InputCSS p-1 text-muted" placeholder="What needs to be done?" value={inputText} onChange={(event) => setInput(event.target.value)} onKeyDown={(event) => {
-          if (event.key === "Enter") {
-            sendData(event);
-          }
-        }} />
-        <ul className="list-group">
-          {toDo.length === 0 ? (
-            <li className="list-group-item bg-light text-muted">No hay tareas, añadir tareas</li>
-          ) : (
-            toDo.map((task, index) => (
-              <li key={index} className="list-group-item d-flex justify-content-between align-items-center text-muted">
-                {task}
-                <button
-                  type="button"
-                  className="btn Delete"
-                  onClick={() => handleDelete(index)}
-                >
-                  <i class="fas fa-trash-alt"></i>
-                </button>
-              </li>
-            ))
-          )}
-        </ul>
-      </div>
-    </form >
+    <div className="container">
+      <form onSubmit={sendData}>
+        <h1 className="text-center">To Do List</h1>
+        <div className="Lista">
+          <input type="text" className="InputCSS p-1 text-muted" placeholder="What needs to be done?" value={inputText} onChange={(event) => setInput(event.target.value)} onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              sendData(event);
+            }
+          }} />
+          <ul className="list-group paper">
+            {toDo.length === 0 ? (
+              <li className="list-group-item bg-light text-muted">No hay tareas, añadir tareas</li>
+            ) : (
+              toDo.map((task, index) => (
+                <li key={index} className="list-group-item d-flex justify-content-between align-items-center text-muted">
+                  {task}
+                  <button
+                    type="button"
+                    className="btn Delete"
+                    onClick={() => handleDelete(index)}
+                  >
+                    <i class="fas fa-trash-alt"></i>
+                  </button>
+                </li>
+              ))
+            )}
+          </ul>
+          <p className="AmountTasks">
+            {toDo.length} {toDo.length === 1 ? "item left" : "items left"}
+          </p>
+        </div>
+      </form >
+    </div>
   );
 };
 
